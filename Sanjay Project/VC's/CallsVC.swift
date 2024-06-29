ebfd0511dcbe708b54874f62c41f7ab88477f4b2
aman <amansachdeva93@gmail.com>
2024-06-29 21:19:28 +0530

    Tabs

---
+++ Sanjay Project/VC's/CallsVC.swift
@@ -0,0 +1,32 @@
+//
+//  CallsVC.swift
+//  Sanjay Project
+//
+//  Created by Amanpreet Singh on 29/06/24.
+//
+
+import UIKit
+
+class CallsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func actionView(_ sender: Any) {
+        print("view your msgs at here.")
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Sanjay Project/VC's/CustomTabBarVC.swift
@@ -0,0 +1,23 @@
+//
+//  CustomTabBarVC.swift
+//  Sanjay Project
+//
+//  Created by Amanpreet Singh on 29/06/24.
+//
+
+import UIKit
+
+class CustomTabBarVC: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        delegate = self
+        
+        if let items = tabBar.items, items.count > 3 {
+                    items[3].isEnabled = false
+                }
+        
+    }
+
+}
+
